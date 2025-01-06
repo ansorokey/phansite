08b3887c3c6a21f8a4031c01e0b07de325a75245
Anton Sorokey <ansorokey@gmail.com>
2025-01-06 13:50:24 -0500

    core message features working

--- client.js
+++ client.js
@@ -1,5 +1,58 @@
+// Client port: 5500
+// Server port: 3000
 const socket = io('http://localhost:3000');
 
-socket.on('chat-msg', data => {
-    console.log(data);
+const msgForm = document.querySelector('#msgForm');
+const msgInput = document.querySelector('#msgInput')
+const chatlog = document.querySelector('#chatlog');
+let userName = 'Joker';
+
+promptUser();
+
+
+// #####
+// FUNCTIONS
+// #####
+
+// Ask user for name
+function promptUser() {
+    appendMsg('You joined');
+    socket.emit('new-user', userName);    
+}
+
+// Adds message to chatlog element
+function appendMsg(msg) {
+    const newMsg = document.createElement('p');
+    newMsg.innerText = msg;
+    chatlog.appendChild(newMsg);
+}
+
+// ##### 
+// SOCKET MESSAGES
+// #####
+
+socket.on('chat-msg', (data) => {
+    appendMsg(`${data.user}: ${data.msg}`);
+})
+
+socket.on('user-connected', user => {
+    appendMsg(user + ' connected')
+})
+
+socket.on('user-disconnected', user => {
+    appendMsg(user + ' disconnected')
+})
+
+// #####
+// EVENT LISTENERS
+// #####
+
+// To server: send-chat-msg
+msgForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevent page reload on form submit
+
+    const msg = msgInput.value; // get message text
+    socket.emit('send-chat-msg', msg); // send to server
+    msgInput.value = ""; // clear input box
+    appendMsg('You: ' + msg); // add message to screen (local)
 })
--- server.js
+++ server.js
@@ -3,8 +3,29 @@ const io = require("socket.io")(3000, {
       origin: "http://localhost:5500",
       methods: ["GET", "POST"]
     }
-  });
+});
+
+const users = {}; // Each socket connection is unique
 
 io.on('connection', socket => {
-    socket.emit('chat-msg', 'hello world')
+    // To client: chat-msg
+    socket.on('send-chat-msg', msg => {
+        socket.broadcast.emit('chat-msg', {
+            msg,
+            user: users[socket.id]
+        }); //sends data to all but original sender
+    });    
+    
+    // To client: user-connected
+    socket.on('new-user', userName => {
+        users[socket.id] = userName;
+        socket.broadcast.emit('user-connected', userName); //sends data to all but original sender
+    });
+
+    // To client: user-disconnected
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', `${socket.id}`); //sends data to all but original sender
+        delete users[socket.id];
+    });
 });
+
